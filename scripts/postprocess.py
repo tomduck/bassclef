146cbeec8ca255cbb5c707464ac4aaece694fcc3
Thomas Duck <tomduck@tomduck.ca>
2015-09-30 21:14:22 -0300

    Undo.

--- scripts/postprocess.py
+++ scripts/postprocess.py
@@ -111,8 +111,6 @@ def postprocess():
             lines[i] = lines[i][:-1] + ' ' + lines[i+1][3:-5] + '\n'
             lines[i+1] = None
             continue
-        if line.endswith('-->'):
-            lines[i] = line + '\n'
 
 
     ## Output ##
