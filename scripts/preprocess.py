60a56ee344f13b41cb68b497ce85878558048496
Thomas Duck <tomduck@tomduck.ca>
2015-12-07 12:33:15 -0400

    Yet another bug fix.

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -110,7 +110,7 @@ def preprocess(path):
         lines = insert_figure(lines, image, caption)
 
     # Replace macros
-    for i in len(lines):
+    for i in range(len(lines)):
 
         # Clearing line break
         lines[i] = lines[i].replace(
