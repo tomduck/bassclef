b7eed4efc6d80c421358e7f0c090b041058ac53f
Thomas Duck <tomduck@tomduck.ca>
2016-04-08 22:35:19 -0300

    Updated copyright notices.

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #! /usr/bin/env python3
 
+# Copyright 2015, 2016 Thomas J. Duck <tomduck@tomduck.ca>
+
+# This file is part of bassclef.
+#
+#  Bassclef is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License verson 3 as
+#  published by the Free Software Foundation.
+#
+#  Bassclef is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with bassclef.  If not, see <http://www.gnu.org/licenses/>.
+
 """setup.py - setup script for bassclef."""
 
 import sys
