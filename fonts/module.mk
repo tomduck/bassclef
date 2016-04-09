0a18a2e1cd427be06aef6011f4c4984846477f39
Thomas Duck <tomduck@tomduck.ca>
2016-04-08 21:11:46 -0300

    Added copyright notices.

--- fonts/module.mk
+++ fonts/module.mk
@@ -1,4 +1,21 @@
 
+# Copyright 2015, 2016 Thomas J. Duck <tomduck@tomduck.ca>
+
+# This file is part of bassclef.
+#
+#  bassclef is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License verson 3 as
+#  published by the Free Software Foundation.
+#
+#  bassclef is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with bassclef.  If not, see <http://www.gnu.org/licenses/>.
+
+
 # Source files ----------------------------------------------------------------
 
 SOURCE_OPENSANS_FONTS =  $(wildcard submodules/open-sans/fonts/*/*)
