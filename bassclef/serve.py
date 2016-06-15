5b8a4816c6a574bd77204466d08899d7c19fc6e8
Thomas Duck <tomduck@tomduck.ca>
2016-06-15 13:17:30 -0300

    More carefully shut down server.

--- bassclef/serve.py
+++ bassclef/serve.py
@@ -29,21 +29,26 @@ from bassclef.util import printline
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(('', PORT), Handler)
-
-# Catch ^C and exit gracefully
-def signal_handler(signal, frame):
-    printline('\n')
-    httpd.shutdown()
-    httpd.server_close()
-    sys.exit(0)
-signal.signal(signal.SIGINT, signal_handler)
-
 def serve(args):
+    """Runs a test server."""
+
     os.chdir('www')
+
+    # Set up the server
+    Handler = http.server.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(('', PORT), Handler)
+    httpd.allow_reuse_address = True
+    
     printline('Serving at http://127.0.0.1:%d/ (^C to exit)...\n'%PORT)
+
+    # Catch ^C and exit gracefully
+    def signal_handler(signal, frame):
+        printline('\n')
+        httpd.shutdown()
+        httpd.server_close()
+    signal.signal(signal.SIGINT, signal_handler)
+
     thread = threading.Thread(target=httpd.serve_forever)
     thread.start()
 
-    httpd.serve_forever()
+    sys.exit(0)
